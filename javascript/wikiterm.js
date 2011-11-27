76dc18140a9f1323c42d50c17fc0e2e1cb987108
Peter Brindisi <peter.brindisi@gmail.com>
2011-11-27 15:37:47 +0100

    adds map of special wikipedia terms to javascript in order to simplify wikiterm links

--- javascript/wikiterm.js
+++ javascript/wikiterm.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-  $("a.wikiterm").each(function(elem) {
-    var url = $(this).data("url") || $(this).html();
-    $(this).attr("href", "http://en.wikipedia.org/wiki/" + url);
-    $(this).attr("target", "_blank");
-  });
+  var WikiTerms = function() {
+    var special = {
+      c   : "C_(programming_language)",
+      gcc : "GNU_Compiler_Collection",
+      vim : "vim_(text_editor)"
+    };
+
+    this.wikify = function() {
+      $("a.wikiterm").each(function(elem) {
+
+        var url = $(this).data("url") || $(this).html().replace(" ", "_");
+
+        url = special[url.toLowerCase()] || url;
+
+        $(this).attr("href", "http://en.wikipedia.org/wiki/" + url);
+        $(this).attr("target", "_blank");
+      });
+    };
+  };
+  
+  (new WikiTerms()).wikify();
 });
